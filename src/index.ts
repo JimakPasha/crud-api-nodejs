a5d578e1f994f47ffc505f8c59fbc334e6fd05fa
pavel_hladki <pavel_hladki@senla.eu>
2024-02-10 23:01:51 +0300

    feat: add error server

--- src/index.ts
+++ src/index.ts
@@ -17,99 +17,108 @@ interface IUser {
 let users: IUser[] = [];
 
 const server = createServer((req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-
-  const { method, url } = req;
-
-  if (url === '/api/users') {
-    switch (method) {
-      case 'GET':
-        res.writeHead(200);
-        res.end(JSON.stringify(users));
-        break;
-      case 'POST':
-        let body: string = '';
-
-        req
-        .on('data', (chunk) => {
-          body+= chunk.toString();
-        })
-        .on('end', () => {
-          const { username, age, hobbies }: Omit<IUser, 'id'> = JSON.parse(body);
-
-          if (!username || !age) {
-            res.writeHead(400);
-            res.end(JSON.stringify({ error: 'Username and age are required' }));
-          } else {
-            const newUser = {
-              id: uuid.v4(),
-              username,
-              age,
-              hobbies: hobbies || [],
-            }
-            users.push(newUser);
-    
-            res.writeHead(201);
-            res.end(JSON.stringify(newUser));
-          }
-        });
-        break;
-    }
-  }
-
-  if (url?.startsWith('/api/users/')) {
-    const userId = url.split('/').at(-1);
-
-    if (!userId || !uuid.validate(userId)) {
-      res.writeHead(400);
-      res.end(JSON.stringify({ error: `Id isn't uuid` }));
-      return;
-    }
-
-    const user = users.find(({ id }) => id === userId);
-
-
-    if (!user) {
-      res.writeHead(404);
-      res.end(JSON.stringify({ error: 'User not found' }));
-    } else {
+  try {
+    res.setHeader('Content-Type', 'application/json');
+  
+    const { method, url } = req;
+  
+    if (url === '/api/users') {
       switch (method) {
         case 'GET':
           res.writeHead(200);
-          res.end(JSON.stringify(user));
+          res.end(JSON.stringify(users));
           break;
-        case 'PUT':
+        case 'POST':
           let body: string = '';
-
+  
           req
           .on('data', (chunk) => {
             body+= chunk.toString();
           })
           .on('end', () => {
-            const { username, age, hobbies }: Partial<IUser> = JSON.parse(body);
+            const { username, age, hobbies }: Omit<IUser, 'id'> = JSON.parse(body);
   
-            const updatedUser = {
-              id: user.id,
-              username: username || user.username,
-              age: age || user.age,
-              hobbies: hobbies || user.hobbies,
+            if (!username || !age || !hobbies) {
+              res.writeHead(400);
+              res.end(JSON.stringify({ error: 'Username, age and hobbies are required' }));
+            } else {
+              const newUser = {
+                id: uuid.v4(),
+                username,
+                age,
+                hobbies,
+              }
+              users.push(newUser);
+      
+              res.writeHead(201);
+              res.end(JSON.stringify(newUser));
             }
-
-            users = users.map((user) => user.id === userId ? updatedUser : user);
-
-            res.writeHead(200);
-            res.end(JSON.stringify(updatedUser));
-          })
+          });
           break;
-        case 'DELETE':
-          users = users.filter(({ id }) => id !== userId);
-          res.writeHead(204);
-          res.end();
-        break;
       }
+      return;
     }
-
-    return;
+  
+    if (url?.startsWith('/api/users/')) {
+      const userId = url.split('/').at(-1);
+  
+      if (!userId || !uuid.validate(userId)) {
+        res.writeHead(400);
+        res.end(JSON.stringify({ error: `Id isn't uuid` }));
+        return;
+      }
+  
+      const user = users.find(({ id }) => id === userId);
+  
+  
+      if (!user) {
+        res.writeHead(404);
+        res.end(JSON.stringify({ error: 'User not found' }));
+      } else {
+        switch (method) {
+          case 'GET':
+            res.writeHead(200);
+            res.end(JSON.stringify(user));
+            break;
+          case 'PUT':
+            let body: string = '';
+  
+            req
+            .on('data', (chunk) => {
+              body+= chunk.toString();
+            })
+            .on('end', () => {
+              const { username, age, hobbies }: Partial<IUser> = JSON.parse(body);
+    
+              const updatedUser = {
+                id: user.id,
+                username: username || user.username,
+                age: age || user.age,
+                hobbies: hobbies || user.hobbies,
+              }
+  
+              users = users.map((user) => user.id === userId ? updatedUser : user);
+  
+              res.writeHead(200);
+              res.end(JSON.stringify(updatedUser));
+            })
+            break;
+          case 'DELETE':
+            users = users.filter(({ id }) => id !== userId);
+            res.writeHead(204);
+            res.end();
+          break;
+        }
+      }
+  
+      return;
+    }
+  
+    res.writeHead(404);
+    res.end(JSON.stringify({ error: `${url} not found` }));
+  } catch {
+    res.writeHead(500);
+    res.end(JSON.stringify({ error: `Internal Server Error` }));
   }
 });
 
