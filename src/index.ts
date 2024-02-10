f9d1a2eb8d6c0317a36917b758de3e70ac6202c7
pavel_hladki <pavel_hladki@senla.eu>
2024-02-10 17:05:28 +0300

    feat: add GET users, user, POST users

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,93 @@
+
 import { createServer } from 'http';
+
+import { v4 as uuidv4 } from 'uuid';
+
 import 'dotenv/config';
 
 const PORT = process.env.PORT;
 
+interface IUser {
+  id: string;
+  username: string;
+  age: number;
+  hobbies: string[],
+}
+
+let users: IUser[] = [
+  {
+    id: '1',
+    username: 'Jack',
+    age: 30,
+    hobbies: ['music', 'sport'],
+  },
+];
+
 const server = createServer((req, res) => {
-  res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  console.log('test');
-  res.end();
+
+  const { method, url } = req;
+
+  if (url === '/api/users') {
+    switch (method) {
+      case 'GET':
+        res.writeHead(200);
+        res.end(JSON.stringify(users));
+        break;
+      case 'POST':
+        let body: string = '';
+
+        req
+        .on('data', (chunk) => {
+          body+= chunk.toString();
+        })
+        .on('end', () => {
+          const { username, age, hobbies }: IUser = JSON.parse(body);
+
+          if (!username || !age) {
+            res.writeHead(400);
+            res.end(JSON.stringify({ error: 'Username and age are required' }));
+          } else {
+            const newUser = {
+              id: uuidv4(),
+              username,
+              age,
+              hobbies: hobbies || [],
+            }
+            users.push(newUser);
+    
+            res.writeHead(201);
+            res.end(JSON.stringify(newUser));
+          }
+        });
+        break;
+    }
+  }
+
+  if (url?.startsWith('/api/users/')) {
+    const userId = url.split('/').at(-1);
+    const user = users.find(({ id }) => id === userId);
+
+    if (!user) {
+      res.writeHead(404);
+      res.end(JSON.stringify({ error: 'User not found' }));
+    } else {
+      switch (method) {
+        case 'GET':
+          res.writeHead(200);
+          res.end(JSON.stringify(user));
+          break;
+        case 'PUT':
+        
+        break;
+        case 'DELETE':
+        
+        break;
+      }
+    }
+
+    return;
+  }
 });
 
 server.listen(PORT, () => {
