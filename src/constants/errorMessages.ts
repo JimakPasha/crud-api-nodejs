c016bea5b89f25a872ce5ba4a2411527d8e87bc0
pavel_hladki <pavel_hladki@senla.eu>
2024-02-11 23:35:46 +0300

    fix: delete extra errorMessage

--- src/constants/errorMessages.ts
+++ src/constants/errorMessages.ts
@@ -2,7 +2,6 @@ export const errorMessages = {
   requireFields: 'Username, age and hobbies are required',
   errorUuid: `Id isn't uuid`,
   userNotFound: 'User not found',
-  notFound: 'not found',
   internalServer: 'Internal Server Error',
   notFoundUrl: (url: string) => `Url: ${url} not found`,
 };
